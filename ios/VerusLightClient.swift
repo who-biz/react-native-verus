b2ccd54545bf6f688ef2fd83e42d10d61c602a15
who-biz <37732338+who-biz@users.noreply.github.com>
2026-06-22 20:12:25 -0400

    Modify timestamp for VRSC change txs

--- ios/VerusLightClient.swift
+++ ios/VerusLightClient.swift
@@ -360,11 +360,13 @@ class VerusLightClient: RCTEventEmitter {
 
             do {
               var txData = await wallet.parseTx(tx: tx)
+              /*
               if tx.isPending(currentHeight: currentHeight) {
                 txData.status = "pending"
               } else {
                 txData.status = "confirmed"
               }
+              */
               out.append(txData.nsDictionary)
             }
           }
@@ -1010,6 +1012,15 @@ class WalletSynchronizer: NSObject {
       blockTimeInSeconds: Int(tx.blockTime ?? 0),
       value: String(describing: abs(tx.value.amount))
     )
+
+    if tx.isPending(currentHeight: currentHeight) {
+        txData.status = "pending"
+        // workaround for VRSC change timestamp
+        txData.blockTimeInSeconds = Int(Date().timeIntervalSince1970)
+    } else {
+        txData.status = "confirmed"
+    }
+
     /*if tx.raw != nil {
       txData.raw = tx.raw!.hexEncodedString()
     }*/
